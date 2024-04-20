7fda9e3f16c66e3f88878d22f534e9407564e49f
jason lam <khjasonlam@gmail.com>
2024-04-20 19:26:15 +0900

    :fire: Remove script tag

--- src/CommonTools.php
+++ src/CommonTools.php
@@ -16,7 +16,6 @@
     return $mode;
   }
 ?>
-<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script> -->
 <div class="sticky-top container-fluid bg-info py-3">
   <div class="row g-2">
     <div class="col-2">
