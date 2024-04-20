5fcfe9bac9abed5b0c82aaf9ef5a44c06040be83
jason lam <khjasonlam@gmail.com>
2024-04-20 19:30:42 +0900

    :hammer: Check login status using session

--- src/LoginStatus.php
+++ src/LoginStatus.php
@@ -1,31 +1,15 @@
 <?php
-  session_start();
-  define("LOGIN_PAGE", "Login.php");
-  
-  if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    if (isset($_POST['logoutSubmit'])) {
-      session_unset();
-      header("Location:". LOGIN_PAGE);
-      exit;
-    }
+session_start();
+define("LOGIN_PAGE", "Login.php");
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+  if (isset($_POST['logoutSubmit'])) {
+    session_unset();
+    header("Location:". LOGIN_PAGE);
+    exit;
   }
-  
-  try {    
-    $checkStatusSql = "SELECT userId FROM Users WHERE userId = ?";
-    $stmt = $conn->prepare($checkStatusSql);
-    
-    $stmt->bindValue(1, $_SESSION["userId"]);
-    $stmt->execute();
-    
-    $user = $stmt->rowCount();
-    
-    if ($user === 1) {
-      $loginStatus = true;
-    } else {
-      $loginStatus = false;
-      header("Location:" . LOGIN_PAGE);
-    }
-  } catch (PDOException $e) {
-    var_dump($e);
-  }
-?>
+}
+
+if (!isset($_SESSION["userId"])) {
+  header("Location:" . LOGIN_PAGE);
+}
--- src/SelectProfileItem.php
+++ src/SelectProfileItem.php
@@ -1,64 +1,63 @@
 <?php
-  session_start();
-  
-  if (isset($_GET["targetUserId"])) {
-    $displayUserId = $_GET["targetUserId"];
-  } else if (isset($_POST["messageUserId"])) {
-    $displayUserId = $_POST["messageUserId"];
-  } else {
-    $displayUserId = $_SESSION["userId"];
-  }
-  
-  try {
-    $profileSql = 
-      "SELECT u.userId, u.username, u.gender, u.age, 
-      u.bloodType, u.location, u.interests,
-      u.description, u.height, u.weight, u.education, 
-      u.occupation, u.smokingHabits, u.drinkingHabits,
-      up.pictureContents, up.pictureType
-      FROM Users u LEFT JOIN User_Pictures up 
-      ON u.userId = up.userId WHERE u.userId = ?";
-      
-    $stmt = $conn->prepare($profileSql);
+session_start();
+
+if (isset($_GET["targetUserId"])) {
+  $displayUserId = $_GET["targetUserId"];
+} else if (isset($_POST["messageUserId"])) {
+  $displayUserId = $_POST["messageUserId"];
+} else {
+  $displayUserId = $_SESSION["userId"];
+}
+
+try {
+  $profileSql = 
+    "SELECT u.userId, u.username, u.gender, u.age, 
+    u.bloodType, u.location, u.interests,
+    u.description, u.height, u.weight, u.education, 
+    u.occupation, u.smokingHabits, u.drinkingHabits,
+    up.pictureContents, up.pictureType
+    FROM Users u LEFT JOIN User_Pictures up 
+    ON u.userId = up.userId WHERE u.userId = ?";
     
-    $stmt->bindValue(1, $displayUserId);
-    $stmt->execute();
-    
-    $result = $stmt->fetch(PDO::FETCH_ASSOC);
-    
-  } catch (PDOException $e) {
-    var_dump($e->getMessage());
-  }
+  $stmt = $conn->prepare($profileSql);
   
-  $username = checkValue($result['username']);
-  $gender = $result['gender'];
-  $age = $result['age'];
-  $bloodType = checkValue($result['bloodType']);
-  $location = checkValue($result['location']);
-  $interests = checkValue($result['interests']);
-  $description = checkValue($result['description']);
-  $height = checkValue($result['height']);
-  $weight = checkValue($result['weight']);
-  $education = checkValue($result['education']);
-  $occupation = checkValue($result['occupation']);
-  $smokingHabits = checkValue($result['smokingHabits']);
-  $drinkingHabits = checkValue($result['drinkingHabits']);
+  $stmt->bindValue(1, $displayUserId);
+  $stmt->execute();
   
-  $pictureContents = $result['pictureContents'];
-  $pictureType = $result['pictureType'];
+  $result = $stmt->fetch(PDO::FETCH_ASSOC);
   
-  $profileArray = [
-    "性別" => $gender, 
-    "年齢" => $age, 
-    "血液型" => $bloodType, 
-    "出身地" => $location, 
-    "趣味" => $interests, 
-    "自己紹介" => $description,
-    "身長" => $height, 
-    "体重" => $weight, 
-    "学歴" => $education,
-    "職業" => $occupation, 
-    "喫煙" => $smokingHabits, 
-    "飲酒" => $drinkingHabits
-  ];
-?>
+} catch (PDOException $e) {
+  var_dump($e->getMessage());
+}
+
+$username = checkValue($result['username']);
+$gender = $result['gender'];
+$age = $result['age'];
+$bloodType = checkValue($result['bloodType']);
+$location = checkValue($result['location']);
+$interests = checkValue($result['interests']);
+$description = checkValue($result['description']);
+$height = checkValue($result['height']);
+$weight = checkValue($result['weight']);
+$education = checkValue($result['education']);
+$occupation = checkValue($result['occupation']);
+$smokingHabits = checkValue($result['smokingHabits']);
+$drinkingHabits = checkValue($result['drinkingHabits']);
+
+$pictureContents = $result['pictureContents'];
+$pictureType = $result['pictureType'];
+
+$profileArray = [
+  "性別" => $gender, 
+  "年齢" => $age, 
+  "血液型" => $bloodType, 
+  "出身地" => $location, 
+  "趣味" => $interests, 
+  "自己紹介" => $description,
+  "身長" => $height, 
+  "体重" => $weight, 
+  "学歴" => $education,
+  "職業" => $occupation, 
+  "喫煙" => $smokingHabits, 
+  "飲酒" => $drinkingHabits
+];
