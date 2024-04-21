57278dfccb444a0765bd8f68f4fe4880b6c490cd
jason lam <khjasonlam@gmail.com>
2024-04-22 01:26:52 +0900

    :hammer: Seperate Register DB process

--- src/pages/Register.php
+++ src/pages/Register.php
@@ -1,6 +1,3 @@
-<?php 
-  session_start();
-?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -13,89 +10,11 @@
   </head>
   <body>
     <?php
-      include_once("../database/Pdo.php");
       include_once("../components/CommonTools.php");
       include_once("../components/CheckInput.php");
-      
-      $error = new ErrorMessage;
-      
-      if ($_SERVER["REQUEST_METHOD"] === "POST") {
-        if (isset($_POST["registerSubmit"])) {
-          $inputValue = isset($_POST["loginId"]) && !empty($_POST["password"]) && 
-            isset($_POST["username"]) && ($_POST["age"] !== "年齢を選択していください") && 
-            isset($_POST["gender"]) && is_uploaded_file($_FILES["profilePicture"]["tmp_name"]);
-          
-          if ($inputValue) {
-            $loginId = testInputValue($_POST["loginId"]);
-            $password = testInputValue($_POST["password"]);
-            $username = testInputValue($_POST["username"]);
-            $age = $_POST["age"];
-            $gender = $_POST["gender"];
-            
-            $pictureName = $_FILES["profilePicture"]["name"];
-            $pictureType = $_FILES["profilePicture"]["type"];
-            $pictureSize = $_FILES["profilePicture"]["size"];
-            $pictureTmpName = $_FILES["profilePicture"]["tmp_name"];
-            $pictureFile = file_get_contents($pictureTmpName);
-            $pictureContents = base64_encode($pictureFile);
-            
-            if ($pictureSize <= MAX_SIZE) {
-              try {
-                $checkUserSql = "SELECT loginId FROM Users WHERE loginId = ?";
-                $stmt = $conn->prepare($checkUserSql);
-                $stmt->bindValue(1, $loginId);
-                $stmt->execute();
-                
-                $result = $stmt->fetch(PDO::FETCH_ASSOC);
-                
-                if (empty($result)) {
-                  $conn->beginTransaction();
-                  
-                  $registerSql = 
-                    "INSERT INTO Users (loginId, password, username, gender, age) 
-                    VALUES (?, ?, ?, ?, ?)";
-                  $stmt = $conn->prepare($registerSql);
-                  
-                  $stmt->bindValue(1, $loginId);
-                  $stmt->bindValue(2, $password);
-                  $stmt->bindValue(3, $username);
-                  $stmt->bindValue(4, $gender);
-                  $stmt->bindValue(5, $age);
-                  $stmt->execute();
-                  
-                  $lastInsertId = $conn->lastInsertId();
-                  
-                  $uploadPictureSql = 
-                    "INSERT INTO User_Pictures (userId, pictureName, pictureType, pictureContents) 
-                    VALUES (?, ?, ?, ?)";
-                  $stmt = $conn->prepare($uploadPictureSql);
-                  
-                  $stmt->bindValue(1, $lastInsertId);
-                  $stmt->bindValue(2, $pictureName);
-                  $stmt->bindValue(3, $pictureType);
-                  $stmt->bindValue(4, $pictureContents);
-                  $stmt->execute();
-                  
-                  $conn->commit();
-                  header("Location: Login.php");
-                } else {
-                  $error->setErrorMessage("このログインIDが登録済みです");
-                } 
-              } catch (PDOException $e) {
-                $error->setErrorMessage("DB登録失敗" . $e->getMessage());
-                $conn->rollback();
-              }
-            } else {
-              $error->setErrorMessage("画像サイズが1Mを超えました");
-            }
-          } else {
-            $error->setErrorMessage("すべてが必須項目");
-          }
-        }
-      }
     ?>
     <div class="container p-4 bg-light">
-      <form class="row g-4" method="POST" action="Register.php">
+      <form class="row g-4" method="POST" action="../database/ProcessRegister.php">
         <!-- username -->
         <div class="col-md-6">
           <label for="loginId" class="form-label">ログインID</label>
@@ -157,7 +76,6 @@
             </div>
           </div>
         </div>
-        <div class="col-12 text-center text-danger"><?php $error->displayErrorMessage();?></div>
         <!-- submit -->
         <div class="col-md-6 d-grid">
           <input 
@@ -172,6 +90,7 @@
             ログイン画面に戻る
           </a>
         </div>
+        <div class="text-center text-danger"><?php displayErrorMessage();?></div>
       </form>
     </div>
   </body>
