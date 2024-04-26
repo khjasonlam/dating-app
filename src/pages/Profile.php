21a88dd173cd7eb286b4206a26a45c2e3874e5e5
jason lam <khjasonlam@gmail.com>
2024-04-27 04:24:02 +0900

    :hammer: Profile item overflow

--- src/pages/Profile.php
+++ src/pages/Profile.php
@@ -15,20 +15,20 @@
       include_once("../database/SelectProfileItem.php");
     ?>
     <div class="container p-4 bg-light">
-      <div class="row">
-        <div class="col-md-6 text-center mb-3">
+      <div class="row mx-3">
+        <div class="col-md-5 text-center">
           <img 
             <?php echo "src='data: $pictureType; base64, $pictureContents'"; ?> 
             alt="profile_picture" class="object-fit-scale border rounded" 
             height="auto" width="auto" style="max-width: 300px;"
           >
         </div>
-        <div class="col-md-6 px-4">
+        <div class="col-md-7 px-4" style="height: 80vh; overflow-y: auto;">
           <div class="row">
-            <div class="col-9 col-md-9 h1 strong">
-              <?php echo $username ?>
+            <div class="col-10 h1 strong">
+              <?php echo $username; ?>
             </div>
-            <div class="col-3 col-md-3">
+            <div class="col-2">
               <?php if ($displayUserId === getUserIdSession()): ?>
                 <a class="float-end" href="EditProfile.php">
                   <img src="../assets/icon/pencil-square.svg" width="32" height="32" class="m-2">
